5f21e064ce6c83a509bd55591e5138faa4628ce5
Tim Zwart <timwaagh@gmail.com>
2023-06-11 02:43:28 +0200

    max recursion implemented

--- dump_var.py
+++ dump_var.py
@@ -1,18 +1,18 @@
 from types import NoneType
 from enum import Enum
+max_recursion = 3
 
-
-def string_var(var):
+def string_var(var, recursion=0):
     typ = type(var)
     retval = ""
     if typ in (str, int, float, bool, NoneType, Enum):
         retval = stringify_primitive(typ, var)
     elif typ == dict:
-        retval = stringify_dict(var)
+        retval = stringify_dict(var, recursion)
     elif typ == list:
-        retval = stringify_list(var)
+        retval = stringify_list(var, recursion)
     elif isinstance(var, object):
-        retval = stringify_class(var)
+        retval = stringify_class(var, recursion)
     return retval
 
 
@@ -33,33 +33,45 @@ def stringify_primitive(typ, var):
     return retval
 
 
-def stringify_dict(var):
+def stringify_dict(var, recursion=0):
     retval = "{"
     for key in var:
-        retval += string_var(key) + " -> " + string_var(var[key]) + ","
+        if recursion < max_recursion:
+            retval += string_var(key, recursion+1) + " -> " + string_var(var[key], recursion+1) + ","
+        else:
+            retval += str(key) + " -> " + str(var[key]) + ","
     if len(var) > 0:
         retval = retval[:-1]  # laatste komma weg
     retval += "}"
     return retval
 
 
-def stringify_list(var):
+def stringify_list(var, recursion=0):
     retval = "["
     for i in range(len(var)):
         if i == len(var) - 1:
-            retval += str(i) + "->" + string_var(var[i])
+            if recursion < max_recursion:
+                retval += str(i) + "->" + string_var(var[i], recursion+1)
+            else:
+                retval += str(i) + "->" + str(var[i])
         else:
-            retval += str(i) + "->" + string_var(var[i]) + ","
+            if recursion < max_recursion:
+                retval += str(i) + "->" + string_var(var[i], recursion+1) + ","
+            else:
+                retval += str(i) + "->" + str(var[i])
     retval += "]"
     return retval
 
 
-def stringify_class(var):
+def stringify_class(var, recursion=0):
     retval = str(type(var)) + ": {["
     fields = [a for a in dir(var) if not a.startswith('__') and not callable(getattr(var, a))]
     for field in fields:
         if hasattr(var, "__dict__"):
-            retval += field + ": " + string_var(vars(var)[field])
+            if recursion < max_recursion:
+                retval += field + ": " + string_var(vars(var)[field], recursion+1)
+            else:
+                retval += field + ": " + str(vars(var)[field])
         else:
             retval += field + ": " + "?"
     retval += "]}"
@@ -67,4 +79,4 @@ def stringify_class(var):
 
 
 def dump_var(var):
-    print(string_var(var))
+    print(string_var(var, 0))
